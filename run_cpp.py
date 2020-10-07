d3592ed5fc28fc93eb1e45e845d84a788e31326d
mpvaitheeswaran <mpvaitheeswaran>
2020-10-07 18:57:42 +0530

    First commit is best commit

---
+++ run_cpp.py
@@ -0,0 +1,5 @@
+
+import os
+print('Compiling...')
+os.system('g++ run_py.cpp -o run_py')
+os.system('./run_py')
---
+++ run_py.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Python is running";
+    system("python run_cpp.py");
+    return 0;
+}
